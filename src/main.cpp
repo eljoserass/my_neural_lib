9d2ca02ac56ce9d0a0a02e4c59f45bdf852d5c31
eljoserass <jose-antonio.rodriguez-assalone@epitech.eu>
2023-12-20 05:44:35 +0000

    [ADD] working prototype of wx+b in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,31 @@
 int main(void) {
     Feedforward neuralNetwork(2, 3, 1);
     std::cout << "hello" << std::endl;
-    const int inpu_d = 2;
-    Eigen::Matrix<float , inpu_d, 3> input {
+    const int rows = 2;
+    const int attr = 3;
+    const int neurons = 2;
+    Eigen::Matrix<float , rows, attr> input {
         {1,2,1},
         {0,3,1}
     };
-    Eigen::Matrix<float , 3, 2> w {
+    Eigen::Matrix<float , attr, neurons> w {
         {1, 1},
         {0.5, 0.5},
         {2,2}
     };
 
-    Eigen::Matrix<float, 2,2> b {{1, 1},{1,1}
-    };
-    std::cout << input * w + b<< std::endl;
+    // Eigen::Matrix<float, 2,2> b {{1, 1},{1,1}
+    // };
+    Eigen::Matrix<float, rows, neurons> result;
+    Eigen::Matrix<float, 1, neurons> b;
+    b << 1.f, 2.f;
+    std::cout << b.transpose() << std::endl;
+    // auto resultt = input * w;
+    // result += b;
+    result = input * w;
+    std::cout << "before b \n" << result << std::endl;
+    // result = input * w +  b.transpose().replicate(input.rows(), 1);
+    result.rowwise() += b;
+    std::cout << result << std::endl;
     return 0;
 }
